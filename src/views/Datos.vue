1f50a1c9d3d00c25aea3200de699205e39b588c2
espinozav18 <exonespinoza@gmail.com>
2021-12-20 20:29:41 -0500

    desafio semana 12

--- src/views/Datos.vue
+++ src/views/Datos.vue
@@ -7,7 +7,7 @@
       <v-data-table
         :headers="headers"
         :items="listaItems"
-        :items-per-page="5"
+        :items-per-page="10"
         class="elevation-1"
         :loading="'!=listaItems.length'"
         loading-text="Cargando...Por favor espere!"
